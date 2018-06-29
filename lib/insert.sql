3c789d3ec8926864d19c56c0f854c9a8fab25ec6
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 18:54:52 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 20, "M", "light brown", "serious", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 10, "M", "light brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 6, "F", "dark brown", "serious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 7, "M", "light brown", "serious", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 13, "F", "dark brown", "serious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch", 2, "F", "light brown", "serious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy", 6, "F", "light brown", "serious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, 20, "M", "black", "aggressive", 0);
