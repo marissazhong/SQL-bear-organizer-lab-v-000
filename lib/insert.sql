cd1850bfd57a0c07b18666b2650f671b4b14ec53
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 18:36:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender) VALUES ("Mr. Chocolate");
-INSERT INTO bears (name, age, gender) VALUES ("Rowdy");
-INSERT INTO bears (name, age, gender) VALUES ("Tabitha");
-INSERT INTO bears (name, age, gender) VALUES ("Sergeant Brown");
-INSERT INTO bears (name, age, gender) VALUES ("Melissa");
-INSERT INTO bears (name, age, gender) VALUES ("Grinch");
-INSERT INTO bears (name, age, gender) VALUES ("Wendy");
+INSERT INTO bears (name, age, gender) VALUES ("Mr. Chocolate", 5, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Rowdy", 4, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Tabitha", 6, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Sergeant Brown", 7, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Melissa", 7, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Grinch", 5, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Wendy", 4, "F");
 INSERT INTO bears (name, age, gender) VALUES (null);
